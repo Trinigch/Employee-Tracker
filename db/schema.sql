dae8cb9567f16000ea3e1ec3718d9f7afeb35292
Trinigch <trinigch@gmail.com>
2024-10-22 16:53:16 -0500

    adding db

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS company_db;
+
 CREATE DATABASE company_db;
 
 \c company_db;
-
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,27 @@
--- Insertar departamentos
+      
+-- Insertar datos en la tabla movies
 INSERT INTO department (name) VALUES 
 ('Engineering'), 
 ('Human Resources'), 
-('Sales');
+('Sales'),
+('Software Engineering'),
+('Electronic Engineering'), 
+('Mecanic Engineering');
 
 -- Insertar roles
 INSERT INTO role (title, salary, department_id) VALUES
-('Software Engineer', 75000, 1),
+('Engineer', 75000, 1),
 ('HR Manager', 60000, 2),
-('Sales Representative', 50000, 3);
+('Sales', 50000, 3),
+('Software Engineering', 75000, 4),
+('Electronic Engineering', 85000, 5), 
+('Mecanic Engineering', 95000, 6);
 
 -- Insertar empleados
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-('John', 'Doe', 1, NULL),
-('Jane', 'Smith', 2, 1),
-('Emily', 'Jones', 3, NULL);
+('John', 'blanch', 5, NULL),
+('Juana', 'Smith', 2, 1),
+('Emily', 'Marones', 4, NULL),
+('Maria', 'Doe', 1, NULL),
+('Elena', 'moritana', 2, 1),
+('Emilia', 'Jones', 4, NULL);
